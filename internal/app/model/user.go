6766ff829502268e712cd492a37b47fb043a6e96
Daniel Genaro <danielgenaro198@gmail.com>
2025-02-24 20:42:28 -0300

    feat(jwt-oauth): generate client_id and secret while creating a new instance of user entity

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type PostUserResponse struct {
 	ID    string `json:"id"`
 	Email string `json:"email"`
@@ -7,8 +9,12 @@ type PostUserResponse struct {
 }
 
 type PostUserRequest struct {
+	Email string `validate:"required" json:"email"`
+	Name  string `validate:"required" json:"name"`
+}
+
+type PostUserLogin struct {
 	Email    string `validate:"required" json:"email"`
-	Name     string `validate:"required" json:"name"`
 	Password string `validate:"required" json:"password"`
 }
 
@@ -16,4 +22,18 @@ type GetUserResponse struct {
 	ID    string `json:"id"`
 	Email string `json:"email"`
 	Name  string `json:"name"`
+}
+
+type UserQueue struct {
+	ClientID string `json:"client_id"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+}
+
+type AuthResponse struct {
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token,omitempty"`
+	TokenType    string    `json:"token_type"`
+	ExpiresIn    int64     `json:"expires_in"`
+	ExpiresAt    time.Time `json:"expires_at"`
 }
--- internal/app/user/entity.go
+++ internal/app/user/entity.go
@@ -2,26 +2,53 @@ package user
 
 import (
 	"go-boilerplate/internal/app/model"
+	"go-boilerplate/pkg/cryptography"
 	"go-boilerplate/pkg/validation"
+
+	"github.com/google/uuid"
 )
 
+type UserInterface interface {
+	Validate() error
+	ValidateName() error
+	ValidateEmail() error
+}
 type User struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Password string `json:"-"`
+	ID           string `json:"id"`
+	Email        string `json:"email"`
+	Name         string `json:"name"`
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
 }
 
-func (user User) Validate() error {
+func NewUser(email, name string) (*User, error) {
+	client, err := cryptography.GenerateOAuthSecrets()
+	if err != nil {
+		return nil, err
+	}
+
+	user := User{
+		ID:           uuid.NewString(),
+		Name:         name,
+		Email:        email,
+		ClientID:     client.ClientID,
+		ClientSecret: client.ClientSecret,
+	}
+
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+
+}
+
+func (user *User) Validate() error {
 	if err := user.ValidateName(); err != nil {
 		return err
 	}
 
 	if err := user.ValidateEmail(); err != nil {
-		return err
-	}
-
-	if err := user.ValidatePassword(); err != nil {
 		return err
 	}
 
@@ -41,22 +68,6 @@ func (user *User) ValidateName() error {
 
 	if len(user.Name) > 120 {
 		return model.ErrInvalidName
-	}
-
-	return nil
-}
-
-func (user *User) ValidatePassword() error {
-	if len(user.Password) == 0 {
-		return model.ErrRequiredPassword
-	}
-
-	if len(user.Password) > 64 || len(user.Password) < 8 {
-		return model.ErrInvalidPasswordLength
-	}
-
-	if valid := validation.ValidatePassword(user.Password); !valid {
-		return model.ErrInvalidPassword
 	}
 
 	return nil
