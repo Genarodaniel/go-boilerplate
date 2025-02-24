ee8c43c3860a7e5ef57c4542843f639f8030c1dc
Daniel Genaro <danielgenaro198@gmail.com>
2025-02-24 20:40:58 -0300

    feat(jwt-oauth): created credentials errors

--- internal/app/model/errors.go
+++ internal/app/model/errors.go
@@ -2,6 +2,7 @@ package model
 
 import "errors"
 
+var ErrInvalidRequest = errors.New("request malformed or invalid")
 var ErrRequiredName error = errors.New("name is required")
 var ErrInvalidName error = errors.New("name max length are 120 characters")
 var ErrRequiredEmail error = errors.New("email is required")
@@ -11,3 +12,5 @@ var ErrRequiredUserID = errors.New("user id is required")
 var ErrRequiredPassword = errors.New("password is required")
 var ErrInvalidPasswordLength = errors.New("password should be of length between 8 and 64 characters")
 var ErrInvalidPassword = errors.New("password require a combination of uppercase, lowercase, numbers, and special characters")
+var ErrRequiredCredentials = errors.New("credentials are required")
+var ErrInvalidCredentials = errors.New("credentials are invalid")
